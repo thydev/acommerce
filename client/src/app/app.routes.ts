85720455ca15f5048a15d26205f3173b51b2d3b2
Javier Gonzalez <javig56@gmail.com>
2018-05-22 11:40:52 -0700

    product review route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
+import { ProductReviewComponent } from './product-review/product-review.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'products/:product', component: ProductReviewComponent },
   { path: 'callback', component: CallbackComponent },
   { path: '**', redirectTo: '' }
 ];
