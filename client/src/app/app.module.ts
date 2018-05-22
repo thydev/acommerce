786ab52002a1c0b09ab1abbd3d7363038080b8c7
Javier Gonzalez <javig56@gmail.com>
2018-05-22 10:43:17 -0700

    added rest of additions

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
+// Angular Bootstrap:
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 // Material Components
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
+    MatMenuModule,
+    MatListModule,
+    MatDividerModule,
+    MatSliderModule,
     MatButtonModule,
     MatCheckboxModule,
     MatCardModule,
@@ -19,16 +25,29 @@ import { HttpService } from './http.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ProductReviewComponent } from './product-review/product-review.component';
+import { ProductReviewDetailsComponent } from './product-review-details/product-review-details.component';
+import { ProductUserReviewComponent } from './product-user-review/product-user-review.component';
+import { ProductReviewListingComponent } from './product-review-listing/product-review-listing.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesComponent
+    MessagesComponent,
+    ProductReviewComponent,
+    ProductReviewDetailsComponent,
+    ProductUserReviewComponent,
+    ProductReviewListingComponent,
+
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
+    FormsModule,
     HttpClientModule,
-
+    MatMenuModule,
+    MatSliderModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
@@ -37,7 +56,9 @@ import { MessagesComponent } from './messages/messages.component';
     MatInputModule,
     MatSnackBarModule,
     MatToolbarModule,
-    MatGridListModule
+    MatGridListModule,
+    MatDividerModule,
+    MatListModule
   ],
 
   providers: [HttpService],
