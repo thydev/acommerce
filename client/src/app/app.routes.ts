485746996c6e170be009b94e2ea21f7640bb6fdb
Javier Gonzalez <javig56@gmail.com>
2018-05-22 22:37:22 -0700

    angular home route fix

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { ProductMainComponent } from './product-main/product-main.component';
 import { LandingComponent } from './landing/landing.component';
 
 export const ROUTES: Routes = [
-  { path: '/', component: LandingComponent},
+  { path: '', component: LandingComponent},
   { path: 'products/:product', component: ProductReviewComponent },
   { path: 'products', component: ProductMainComponent },
   { path: '**', redirectTo: '' }
