f807f9b0885012b84d5fdfaab309111652256397
Alexandra <56olesia65@gmail.com>
2018-05-24 10:20:08 -0700

    front order page is done

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1337;
 const baseUrl = '/api/';
 const express = require('express');
 const bodyParser = require('body-parser');
